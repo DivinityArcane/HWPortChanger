7ddf63faa02de20470d0204564a4e75331ece7ee
Justin Eittreim <eittreim.justin@live.com>
2014-06-29 01:15:23 -0700

    Initial Commit!

---
+++ HWPortChanger/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace HWPortChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SelectFileButton = new System.Windows.Forms.Button();
+            this.PatchFileButton = new System.Windows.Forms.Button();
+            this.PortNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Hammerwatch.exe";
+            this.openFileDialog1.Filter = "Hammerwatch Game Binary|Hammerwatch.exe";
+            this.openFileDialog1.Title = "Select your Hammerwatch game binary";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // SelectFileButton
+            // 
+            this.SelectFileButton.Location = new System.Drawing.Point(114, 12);
+            this.SelectFileButton.Name = "SelectFileButton";
+            this.SelectFileButton.Size = new System.Drawing.Size(260, 23);
+            this.SelectFileButton.TabIndex = 0;
+            this.SelectFileButton.Text = "Select File";
+            this.SelectFileButton.UseVisualStyleBackColor = true;
+            this.SelectFileButton.Click += new System.EventHandler(this.SelectFileButton_Click);
+            // 
+            // PatchFileButton
+            // 
+            this.PatchFileButton.Enabled = false;
+            this.PatchFileButton.Location = new System.Drawing.Point(245, 40);
+            this.PatchFileButton.Name = "PatchFileButton";
+            this.PatchFileButton.Size = new System.Drawing.Size(129, 23);
+            this.PatchFileButton.TabIndex = 3;
+            this.PatchFileButton.Text = "Patch it!";
+            this.PatchFileButton.UseVisualStyleBackColor = true;
+            this.PatchFileButton.Click += new System.EventHandler(this.PatchFileButton_Click);
+            // 
+            // PortNumber
+            // 
+            this.PortNumber.Enabled = false;
+            this.PortNumber.Location = new System.Drawing.Point(149, 42);
+            this.PortNumber.Name = "PortNumber";
+            this.PortNumber.Size = new System.Drawing.Size(90, 20);
+            this.PortNumber.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 999;
+            this.label1.Text = "Port:";
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(12, 68);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.ReadOnly = true;
+            this.Output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Output.Size = new System.Drawing.Size(460, 82);
+            this.Output.TabIndex = 4;
+            this.Output.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 162);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PortNumber);
+            this.Controls.Add(this.PatchFileButton);
+            this.Controls.Add(this.SelectFileButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hammerwatch Port Changer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button SelectFileButton;
+        private System.Windows.Forms.Button PatchFileButton;
+        private System.Windows.Forms.TextBox PortNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Output;
+    }
+}
+
---
+++ HWPortChanger/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HWPortChanger
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// This is the version number of the actual program.
+        /// </summary>
+        private string version = "1.0";
+        /// <summary>
+        /// This is the original Hammerwatch port, which we're searching for to replace.
+        /// While it should only be an unsigned 2 byte integer (ushort), it's a signed 4 byte integer (int).
+        /// </summary>
+        private int original_port = 9995; 
+        /// <summary>
+        /// This is a string containing the filename that the end-user selected.
+        /// </summary>
+        private string filename;
+
+        public Form1 ()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This method logs a message to the output console (textbox) in the main form
+        /// </summary>
+        /// <param name="msg">message to be output</param>
+        private void Log (string msg)
+        {
+            if (Output.InvokeRequired)
+            {
+                Output.Invoke(new Action(() => Log(msg)));
+            }
+            else
+            {
+                Output.AppendText(msg + Environment.NewLine);
+            }
+        }
+
+        /// <summary>
+        /// Clicking the "Select File" button triggers the OpenFileDialog
+        /// </summary>
+        private void SelectFileButton_Click (object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        /// <summary>
+        /// Upon selecting a valid file, we store the filename and wait for a port entry.
+        /// </summary>
+        private void openFileDialog1_FileOk (object sender, CancelEventArgs e)
+        {
+            SelectFileButton.Enabled = false;
+            PortNumber.Enabled = true;
+            PatchFileButton.Enabled = true;
+            this.filename = openFileDialog1.FileName;
+            Log("Using binary: " + this.filename);
+            Log("Input the port number between 1 and 65535 you would like to use, then click 'Patch It!'");
+        }
+
+        /// <summary>
+        /// Assuming the end-user selected a new port, we look for positions to overwrite.
+        /// </summary>
+        private void PatchFileButton_Click (object sender, EventArgs e)
+        {
+            if (!File.Exists(this.filename))
+            {
+                Log("Whoa! The file you selected has been moved or deleted! Try again!");
+                PatchFileButton.Enabled = false;
+                PortNumber.Enabled = false;
+                SelectFileButton.Enabled = true;
+                return;
+            }
+
+            ushort port = 0;
+            ushort.TryParse(PortNumber.Text, out port);
+
+            if (port == 0)
+            {
+                Log("Please enter a valid port number between 1 and 65535!");
+                return;
+            }
+
+            Log("Okay, using port: " + port.ToString());
+            string newfile = this.filename + ".bak";
+
+            // Ensure we don't overwrite backups
+            for (byte b = 0; b < 255; b++)
+            {
+                if (!File.Exists(newfile + b.ToString()))
+                {
+                    newfile = newfile + b.ToString();
+                    break;
+                }
+            }
+
+            Log("Backing up the binary to: " + newfile);
+
+            try
+            {
+                File.Copy(this.filename, newfile);
+                Log("Opening binary for reading/writing...");
+
+                // Woo, nested 'using' blocks!
+                using (Stream filestream = new FileStream(filename, FileMode.Open))
+                {
+                    using (BinaryReader reader = new BinaryReader(filestream))
+                    {
+                        using (BinaryWriter writer = new BinaryWriter(filestream))
+                        {
+                            Log("Searching for positions to overwrite...");
+                            int p = 0;
+
+                            // Loop through the file checking for the port in question
+                            for (long pos = 0; pos < filestream.Length; pos++)
+                            {
+                                // Make sure we won't go out of bounds while reading a 4 byte integer
+                                if (pos + 4 < filestream.Length)
+                                {
+                                    filestream.Seek(pos, SeekOrigin.Begin);
+                                    p = reader.ReadInt32();
+
+                                    // Check if the int at the current index matches our target port
+                                    if (p == this.original_port)
+                                    {
+                                        Log("Overwriting port at index " + pos.ToString() + " to: " + port.ToString());
+                                        filestream.Seek(-4, SeekOrigin.Current);
+                                        writer.Write(port);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception E)
+            {
+                Log("Unfortunately, something went wrong: " + E.Message);
+                return;
+            }
+
+            Log("We're done!");
+
+            PatchFileButton.Enabled = false;
+            PortNumber.Enabled = false;
+        }
+
+        /// <summary>
+        /// Some general information to be displayed on startup.
+        /// </summary>
+        private void Form1_Load (object sender, EventArgs e)
+        {
+            Log("Hammerwatch Port Changer v" + this.version + " by DivinityArcane <eittreim.justin@live.com>");
+            Log("Source: http://github.com/DivinityArcane/HWPortChanger");
+            Log("Note that this changes the port used for hosting and joining games.");
+            Log("The client will still show 'Port: 9995' but the actual port will be changed!");
+            Log("Please select your Hammerwatch.exe file!");
+        }
+    }
+}
